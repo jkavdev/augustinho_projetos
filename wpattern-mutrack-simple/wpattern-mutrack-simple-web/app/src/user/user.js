4e7966b6f739c668e6e8023e603f7b5457138138
jkavdev <jhonatankolen@gmail.com>
2016-12-17 13:51:23 -0200

    Parte 4 - Mutrack Simple - AngularJS - (Listagem dos Usuários)(Spring Boot + AngularJS)
    	Consumindo serviço da api de listagem de usuários e exibindo dados.

--- wpattern-mutrack-simple/wpattern-mutrack-simple-web/app/src/user/user.js
+++ wpattern-mutrack-simple/wpattern-mutrack-simple-web/app/src/user/user.js
@@ -1,6 +1,12 @@
 'use strict';
 
 angular.module('mutrack')
-  .controller('UserController', function($scope) {
-    $scope.user = 'JKAVDEV';
+  .controller('UserController', function($scope, $http) {
+    $scope.users = [];
+    
+    $http.get('http://localhost:8086/api/private/user')
+    	.then(function(response){
+    		$scope.users = response.data;
+    	});
+    
   });
