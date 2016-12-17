5067764e06042f0432ca059760bac3b1ac6be541
jkavdev <jhonatankolen@gmail.com>
2016-12-17 14:26:19 -0200

    Parte 5 - Mutrack Simple - AngularJS - (Explicação sobre Serviços)(Spring Boot + AngularJS)
    	Adicionando serviço(util.service.js) que será usado pelos controllers
    js

---
+++ wpattern-mutrack-simple/wpattern-mutrack-simple-web/app/src/utils/util.service.js
@@ -0,0 +1,48 @@
+'use strict';
+
+angular.module('mutrack')
+
+//HttpRequestService - apenas realizará requisição http
+//informando quatro parâmetros - url, o tipo de requisição, conteúdo se houver e método status retorno 
+  .service('HttpRequestService', function($http) {
+    return function(url, method, data, callback) {
+      var requestParams = {
+        method: method,
+        url: url,
+        headers: { 'Content-Type': 'application/json' },
+        data: data
+      };
+
+      $http(requestParams).then(
+        function successCallback(response) {
+          callback && callback(response.data);
+        },
+        function errorCallback() {
+        });
+    };
+  })
+  .service('RestService', function(HttpRequestService) {
+    var restFactory = {};
+
+    // Find all data.
+    restFactory.find = function(url, callback) {
+    	HttpRequestService(url, 'GET', {}, callback);
+    };
+
+    // Aadd a new data.
+    restFactory.add = function(url, data, callback) {
+    	HttpRequestService(url, 'POST', data, callback);
+    };
+
+    // Edit a data.
+    restFactory.edit = function(url, data, callback) {
+    	HttpRequestService(url, 'PUT', data, callback);
+    };
+
+    // Delete a data.
+    restFactory.delete = function(url, data, callback) {
+    	HttpRequestService(url, 'DELETE', data, callback);
+    };
+
+    return restFactory;
+  });
