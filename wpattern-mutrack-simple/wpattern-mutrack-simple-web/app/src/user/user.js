eb2fb379e1cddb18bd00a9ba794128949a934527
jkavdev <jhonatankolen@gmail.com>
2016-12-19 02:40:21 -0200

    Parte 8 - Mutrack Simple - AngularJS - (Finalizando a Adição e Edição)(Spring Boot + AngularJS) - Implementação da exclusão e edição do usuário

--- wpattern-mutrack-simple/wpattern-mutrack-simple-web/app/src/user/user.js
+++ wpattern-mutrack-simple/wpattern-mutrack-simple-web/app/src/user/user.js
@@ -21,10 +21,26 @@ angular.module('mutrack').controller(
 					$scope.users.splice($scope.users.indexOf(user), 1);
 				});
 			};
+			
+			$scope.editUser = function(user){
+				$scope.user = angular.copy(user);
+				$scope.show();
+			};
 		
 			$scope.saveUser = function(user){
 				if(user.id){
-					
+					RestService.edit('http://localhost:8086/api/private/user', user, function(response){
+						delete user.password;
+
+						//Buscando objeto na lista e atribuindo ao objeto encontrado usuário alterado
+						for (var i = 0; i < $scope.users.length; i++) {
+							if($scope.users[i].id === user.id) { 
+								$scope.users[i] = user;
+							}
+						}
+
+						$scope.hide();
+					});	
 				} else {
 					RestService.add('http://localhost:8086/api/private/user', user, function(response){
 						$scope.users.push(response);
